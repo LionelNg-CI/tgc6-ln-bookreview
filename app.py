0e11bc3882100643e6179c9cdaa63e2e30d30c59
LionelNg-CI <lionel.ng.y.c@gmail.com>
2020-08-10 08:40:10 +0000

    add add book template

--- app.py
+++ app.py
@@ -8,7 +8,29 @@ load_dotenv()
 app = Flask(__name__)
 
 MONGO_URI = os.environ.get('MONGO_URI')
-CLOUD_NAME = os.environ.get("CLOUD_NAME")
+client = pymongo.MongoClient(MONGO_URI)
+
+# define my db_name
+DB_NAME = "book_review"
+
+# The Home Route
+# Display all the book listing
+@app.route('/')
+def home():
+    bk = client[DB_NAME].books.find()
+    return render_template('home.template.html', books=bk)
+
+@app.route('/add/book')
+def show_create_form():
+    return render_template('add_book.template.html')
+
+@app.route('/add/book', methods=['POST'])
+def create_task():
+    # extract information from the form
+    title = request.form.get('title')
+    author = request.form.get('author')
+    comments = request.form.get('comments')
+
 
 # "magic code" -- boilerplate
 if __name__ == '__main__':
