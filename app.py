09bd42d15f38ee08bcfd240b71c3a78df9a35956
LionelNg-CI <lionel.ng.y.c@gmail.com>
2020-08-11 01:44:02 +0000

    working on styling and html files

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def process_add_book():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=True),
